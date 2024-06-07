085e7357a7390d59b896364fe140989a5c0c8614
Endz <ivanchukovarsen@yandex.ru>
2024-06-07 13:52:41 +0300

    первый норм коммит - начало
    Создал приложение main, подключил шаблоны и статику, сделал роутинг, но не полностью

--- main/views.py
+++ main/views.py
@@ -3,13 +3,18 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Page content!',
-        'is_auth': True,
-        'list': [2, 1],
-        'dict': {'a': 1, 'b': 2}
+        'title': 'Главная',
+        'content': 'Здравствуйте! '
+                   'Мы рады приветствовать вас на сайте интернет-магазина «Системник»! '
+                   'У нас вы можете приобрести компьютеры, комплектующие и аксессуары к ним. '
+                   'Так же вы можете собрать конфигурацию комьютера на нашем сайте. '
+                   'Если у вас возникнут вопросы, наши консультанты с радостью помогут вам. Приятных покупок!',
     }
-    return render(request, 'main\index.html', context)
+    return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('about page')
+    context = {
+        'title': 'О нас',
+        'content': 'Данный проект является дипломной работой. В дальнейшем буду улучшать, добавлять новый функционал.'
+    }
+    return render(request, 'main/about.html', context)
