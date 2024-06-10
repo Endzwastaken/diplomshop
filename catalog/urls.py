c9188312fe032ac76101330de9f390e178b8b2bb
Endz <ivanchukovarsen@yandex.ru>
2024-06-10 13:11:32 +0300

    #5 filters and pagination
    Добавил фильтры и сделал правильную пагинацию на сайте

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,6 +4,6 @@ from catalog import views as catalog
 app_name = 'catalog'
 
 urlpatterns = [
-    path('', catalog.catalog, name='mainpage'),
+    path('<slug:category_slug>/', catalog.catalog, name='mainpage'),
     path('product/<slug:product_slug>/', catalog.product, name='product')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,32 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
-from catalog.models import Categories, Products
+from catalog.models import Products
 
 
-def catalog(request):
+def catalog(request, category_slug):
 
-    products = Products.objects.all()
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
+    page = int(request.GET.get('page', 1))
+
+    if category_slug == 'all':
+        products = Products.objects.all()
+    else:
+        products = Products.objects.filter(category__slug=category_slug)
+
+    if on_sale:
+        products = products.filter(discount__gt=0)
+
+    if order_by and order_by != 'default':
+        products = products.order_by(order_by)
+
+    paginator = Paginator(products, 6)
+    current_page = paginator.page(page)
 
     context = {
         'title': 'Каталог',
-        'products': products
+        'products': current_page,
+        'slug_url': category_slug,
     }
     return render(request, 'catalog/catalog.html', context)
 
