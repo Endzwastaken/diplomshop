7cff2a95133647ada1cbc1e17f5ff66e95f6af12
Endz <ivanchukovarsen@yandex.ru>
2024-06-13 03:21:41 +0300

    #11 edits
    Сделал отображение заказов пользователя в профиле, убрал кнопку "оформить заказ" где не нужно было, чуть уменьшил кол-во запросов SQL

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
 from django.core.exceptions import ValidationError
+from django.contrib.auth.decorators import login_required
 from django.db import transaction
 from django.shortcuts import render, redirect
 
@@ -7,7 +8,7 @@ from carts.models import Cart
 from orders.forms import CreateOrderForm
 from orders.models import Order, OrderItem
 
-
+@login_required
 def create_order(request):
     if request.method == 'POST':
         form = CreateOrderForm(data=request.POST)
@@ -66,5 +67,6 @@ def create_order(request):
     context = {
         'title': 'Home - Оформление заказа',
         'form': form,
+        'order': True
     }
     return render(request, 'orders/create_order.html', context=context)
